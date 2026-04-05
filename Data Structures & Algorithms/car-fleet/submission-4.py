7d894b1cdfeaa40bbbb4c9830fcce5a3a560fa2f
nihaarn97 <89941572+nihaarn97@users.noreply.github.com>
2026-04-05 14:33:53 -0700

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        steps = []
+        stack = []
+        for idx, val in enumerate(position):
+            time = (target - position[idx])/speed[idx]
+            steps.append((position[idx], time))
+        
+        steps = sorted(steps, key=lambda x: x[0])
+
+        for val in steps:
+            if stack:
+                while stack:
+                    if stack[-1][1] > val[1]:
+                        break
+                    else:
+                        stack.pop()
+            stack.append(val)
+
+        return len(stack)
+        
