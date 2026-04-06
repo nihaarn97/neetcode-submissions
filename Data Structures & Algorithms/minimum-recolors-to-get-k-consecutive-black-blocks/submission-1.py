5f4c7d18f5864e6a94005df1655f29e17ac1a35e
nihaarn97 <89941572+nihaarn97@users.noreply.github.com>
2026-04-06 12:56:41 -0700

    Add: minimum-recolors-to-get-k-consecutive-black-blocks - submission-1

---
+++ Data Structures & Algorithms/minimum-recolors-to-get-k-consecutive-black-blocks/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        countmap = {}
+        for i in range(k):
+            countmap[blocks[i]] = 1 + countmap.get(blocks[i], 0)
+
+        mincount = countmap.get('W', 0)
+        l = 0
+        r = k
+
+        while r<len(blocks):
+            countmap[blocks[r]] = 1+countmap.get(blocks[r], 0)
+            countmap[blocks[l]] = countmap.get(blocks[l])-1
+            mincount = min(mincount, countmap.get('W', 0))
+            l = l+1
+            r = r+1
+
+        return mincount
